149a3a205b3dae34847ac150658667420218d28a
AmoghPatil314 <70491481+AmoghPatil314@users.noreply.github.com>
2020-12-09 18:58:21 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+//Create variables here
+var dog
+var happyDog
+var database
+var foodS
+var foodStock
+function preload()
+{
+  //load images here
+  dogHappy.loadImage("images/dogImg.png");
+  dogHappy.loadImage("images/dogImg1.png");
+}
+
+function setup() {
+  createCanvas(500, 500);
+  dog=createSprite()
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
